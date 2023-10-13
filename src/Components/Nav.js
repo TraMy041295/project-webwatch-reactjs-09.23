d0565c8bbbc489ec5806ac0d191d3715edaef583
Trà My <tramy@MacBook-Pro-cua-Tra.local>
2023-10-13 16:57:26 +0700

    edit filter responesive

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -101,17 +101,17 @@ function Nav(props) {
                 <img src="https://bizweb.dktcdn.net/100/244/305/themes/895013/assets/logo.png?1676340930397" />
             </div>
             {/* --------- cart */}
-            <div className="cart col-md-3 col-3 col-sm-3 col-lg-3 col-xl-3 col-xxl-3">
-                <div className="row cart-item ">
+            <div className="cart row col-md-3 col-3 col-sm-3 col-lg-3 col-xl-3 col-xxl-3">
+                {/* <div className="row cart-item "> */}
                     <span className=" col-md-2 col-4 col-sm-2 col-lg-2 col-xl-2 col-xxl-2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
                         <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                     </svg>
                     </span>
-                    <p className="cart-title row col-md-10 col-1 col-sm-10 col-lg-7 col-xl-6 col-xxl-5">
+                    <p className="cart-title row col-md-10 col-8 col-sm-10 col-lg-7 col-xl-6 col-xxl-5">
                         <span className='col-md-5 col-sm-5 col-lg-5 col-xl-5 col-xxl-5 cart-title-content'><NavLink className='nav-link' to="/gio-hang">Giỏ hàng</NavLink></span>
                         <span className='col-md-2 col-sm-2 col-lg-2 col-xl-2 col-xxl-2' style={{ color: "red" }}><NavLink className='nav-link' to="/gio-hang">({sumQuantity})</NavLink></span>
                     </p>
-                </div>
+                {/* </div> */}
             </div>
         </div>
         <div className='nav-main'>
